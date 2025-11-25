0ba7b6f98539ad380fa46ed692d68e032195f27b
Ian Olmstead <72961956+iolmstead23@users.noreply.github.com>
2025-11-24 18:49:32 -0600

    Dev menu (#1)
    * init main menu feature dev branch
    
    * main file fix
    
    * main and session menu

---
+++ UI/PartyItem.gd
@@ -0,0 +1,25 @@
+extends PanelContainer
+
+signal party_updated(party_id, field, value)
+signal party_removed(party_id)
+
+var party_data = {}
+
+@onready var party_name_label = $MarginContainer/VBoxContainer/HeaderRow/PartyNameLabel
+@onready var robot_count_spinbox = $MarginContainer/VBoxContainer/ConfigRow/RobotCountSpinBox
+
+func set_party_data(data):
+	party_data = data
+	_update_ui()
+
+func _update_ui():
+	if party_name_label:
+		party_name_label.text = party_data.get("name", "Robot Party")
+	if robot_count_spinbox:
+		robot_count_spinbox.value = party_data.get("robot_count", 1)
+
+func _on_robot_count_changed(value):
+	party_updated.emit(party_data.id, "robot_count", int(value))
+
+func _on_remove_pressed():
+	party_removed.emit(party_data.id)
---
+++ UI/SessionSetup.gd
@@ -0,0 +1,61 @@
+extends Control
+
+# Preload the PartyItem scene
+const PartyItem = preload("res://UI/PartyItem.tscn")
+
+# Session data
+var parties = []
+var party_id_counter = 0
+
+@onready var parties_list = $VBoxContainer/ContentArea/MainContent/PartiesSection/PartiesScrollContainer/PartiesList
+
+func _ready():
+	pass
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://UI/MainMenu.tscn")
+
+func _on_add_party_pressed():
+	# Create a new party with default values
+	var party_data = {
+		"id": party_id_counter,
+		"name": "Robot Party " + str(party_id_counter + 1),
+		"type": "robot",
+		"robot_count": 1
+	}
+	party_id_counter += 1
+	parties.append(party_data)
+
+	# Create and add the party UI item
+	var party_item = PartyItem.instantiate()
+	party_item.set_party_data(party_data)
+	party_item.party_updated.connect(_on_party_updated)
+	party_item.party_removed.connect(_on_party_removed)
+	parties_list.add_child(party_item)
+
+func _on_party_updated(party_id, field, value):
+	# Update the party data when values change
+	for party in parties:
+		if party.id == party_id:
+			party[field] = value
+			break
+
+func _on_party_removed(party_id):
+	# Remove party from data
+	for i in range(parties.size()):
+		if parties[i].id == party_id:
+			parties.remove_at(i)
+			break
+
+	# Remove party from UI
+	for child in parties_list.get_children():
+		if child.party_data.id == party_id:
+			child.queue_free()
+			break
+
+func _on_start_session_pressed():
+	# Store session data in an autoload singleton (we'll create this)
+	SessionData.set_session_parties(parties)
+
+	# Start the game
+	get_tree().change_scene_to_file("res://main.tscn")
